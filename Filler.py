f596aee82740e2bce0ab9ef22f654f1e1849dadb
Wahid Shafique <wahidshafique@hotmail.com>
2017-01-03 00:31:20 -0500

    added more stuff

--- Filler.py
+++ Filler.py
@@ -1,5 +1,14 @@
 import string
 import random
 
-def textGenerator(size=6, chars=string.ascii_lowercase):
+def text_generator(size=6, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+def sequence(*functions):
+    def func(*args, **kwargs):
+        return_value = None
+        for function in functions:
+            return_value = function(*args, **kwargs)
+        return return_value
+    return func
--- Main.py
+++ Main.py
@@ -4,14 +4,16 @@ from Tkinter import *
 import Tkinter as tk
 import Filler as filler
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotVisibleException
 from pymongo import MongoClient
 
 #mongodb stuff, grab the db
 client = MongoClient()
 client = MongoClient('localhost', 27017)
 db = client.test
-userprofiles = db.userprofiles
-#print userprofiles.find_one({"username": "yebfly"})
+userProfiles = db.userprofiles
 #endmongo stuff
 
 DESTURL = "http://localhost:8080/#/mSignup/user"
@@ -29,30 +31,51 @@ class Application(tk.Frame, object):
         super(Application, self).__init__(master)
         w = Label(master, text="Choose Option")
         w.pack()
-        self.pack(padx=20,pady=20)
+        self.pack(padx=20, pady=20)
         self.create_widgets()
 
     def create_widgets(self):
-        self.regMentee =  tk.Button(self, text="\nRegister as Mentee\n",
+        self.regMentee = tk.Button(self, text="\nRegister as Mentee\n",
                   command=lambda: self.register_mentee()).pack(side="top")
+
+        self.logMentee = tk.Button(self, text="\nLog in as Mentee\n",
+                                   command=lambda: self.login("mentee@mail.com","password")).pack(side="top")
 
         self.regMentor = tk.Button(self, text="\nRegister as Mentor\n",
                   command=lambda: self.register_mentor()).pack(side="top")
 
+        self.logMentor = tk.Button(self, text="\nLog in as Mentor\n",
+                                   command=lambda: self.login("mentor@mail.com", "password")).pack(side="top")
+
+        tk.Button(self, text="RESET", fg="blue", command=lambda: reset()).pack(side="bottom")
         tk.Button(self, text="QUIT", fg="red",
-                  command=lambda: sequence(root.destroy(), driver.quit())).pack(side="bottom")
+                  command=lambda: filler.sequence(root.destroy(), driver.quit())).pack(side="bottom")
 
     def register_mentee(self):
-        self.register()
-        driver.implicitly_wait(2)#allows for dom to be loaded
-        driver.find_element_by_name('signup-mentee').click()
-        self.become_verified()
+        try:
+            self.register()
+            driver.implicitly_wait(2)#allows for dom to be loaded
+            driver.find_element_by_name('signup-mentee').click()
+            self.become_verified()
+        except NoSuchElementException:
+            print("element does not exist!")
+
+    def login_mentee(self):
+        try:
+            self.login()
+        except NoSuchElementException:
+            print("element does not exist!")
+
 
     def register_mentor(self):
-        self.register()
-        driver.implicitly_wait(2)#allows for dom to be loaded
-        driver.find_element_by_name('signup-mentor').click()
-        self.become_verified()
+        try:
+            self.register()
+            driver.implicitly_wait(2)
+            driver.find_element_by_name('signup-mentor').click()
+            self.become_verified()
+        except NoSuchElementException:
+            print("element does not exist!")
+
 
     def register(self):
         displayName = driver.find_element_by_name("DisplayName")
@@ -60,20 +83,33 @@ class Application(tk.Frame, object):
         password = driver.find_element_by_name("password")
         confPassword = driver.find_element_by_name("confirmPassword")
 
-        self.username = filler.textGenerator()
+        self.username = filler.text_generator()
         displayName.send_keys(self.username)
 
-        EM = filler.textGenerator() + "@mail.com"
+        EM = filler.text_generator() + "@mail.com"
         email.send_keys(EM)
-        PASS = filler.textGenerator()
+        PASS = filler.text_generator()
         password.send_keys(PASS)
         confPassword.send_keys(PASS)
 
         driver.find_element_by_id("terms").click()
-        driver.find_element_by_css_selector("button[ng-click*='SignUpUser']").click()#for angular
+        driver.find_element_by_css_selector("button[ng-click*='SignUpUser']").click()
+
+    def login(self, EM, PASS):
+        try:
+            driver.find_element_by_css_selector("button[ng-click*='open']").click()
+            #overlay takes precendent
+            email = driver.find_element_by_name("email")
+            email.send_keys(EM)
+
+            password = driver.find_element_by_name("password")
+            password.send_keys(PASS)
+            driver.find_element_by_css_selector("button[ng-click*='login']").click()
+        except (NoSuchElementException, ElementNotVisibleException):
+            reset()
 
     def become_verified(self):
-        userprofiles.update_one(
+        userProfiles.update_one(
             {"username": self.username},
                 {
                     "$set": {
@@ -82,25 +118,49 @@ class Application(tk.Frame, object):
                 }
         )
         driver.refresh()
-        driver.find_element_by_css_selector("a[ng-click*='AppendUserData']").click()
+        driver.find_element_by_name("continue").click()
+        driver.implicitly_wait(2)  # allows for dom to be loaded
+        fill_form()
 
 
-def sequence(*functions):
-    def func(*args, **kwargs):
-        return_value = None
-        for function in functions:
-            return_value = function(*args, **kwargs)
-        return return_value
-    return func
+def fill_form():
+    inputs = driver.find_elements_by_xpath("//form[@name='mentorshipForm']//input")
+    print(inputs)
+    for i in inputs:
+        if i.is_displayed():
+            i.send_keys("testing@mail.com")
+
+    driver.find_element_by_xpath("//*[@id='background']/label[1]/span[1]").click()
+
+    selects = driver.find_elements_by_xpath("//form[@name='mentorshipForm']//select")
+    for sels in selects:
+        sels.click()
+        sels.send_keys(Keys.DOWN, Keys.RETURN)
+
+    textAreas = driver.find_elements_by_xpath("//form[@name='mentorshipForm']//textarea")
+    for t in textAreas:
+        if t.is_displayed():
+            t.send_keys(filler.text_generator(300))
+
+
+def reset():
+    if len(driver.find_elements_by_link_text('Logout')) > 0:
+        driver.find_element_by_link_text('Logout').click()
+    driver.get(DESTURL)
+    print "reset"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 result = s.connect_ex(('127.0.0.1', PORT))
 
 root = tk.Tk()
 root.minsize(width=300, height=200)
+root.wm_attributes("-topmost", 1)
 app = Application(master=root)
+
 if result == 0:
     print('socket is open')
     app.mainloop()
-else: sequence(driver.quit(), root.destroy())
+else:
+    filler.sequence(driver.quit(), root.destroy())
+
 print("no open port")
